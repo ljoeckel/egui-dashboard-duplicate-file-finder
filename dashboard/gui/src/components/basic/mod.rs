43aef8b0bfc86fb947243cf394a2ca1696ea11ad
ljoeckel <lothar.joeckel@gmail.com>
2024-08-26 16:09:30 +0200

    Removed unused button_group and display_string

--- dashboard/gui/src/components/basic/mod.rs
+++ dashboard/gui/src/components/basic/mod.rs
@@ -1,8 +1,3 @@
 //! Holds the basic components used in the dashboard.
 
-mod button_group;
-mod display_string;
 pub mod font;
-
-pub use button_group::button_group_clicked;
-pub use display_string::DisplayString;
